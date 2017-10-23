0e9c88d16017d1bcf910bcbf44ff2b2ddb5509d7
wilbai <1058191407@qq.com>
2017-10-23 22:26:31 +0800

    reduce redundant code

--- src/main/java/com/wil/test/Test.java
+++ src/main/java/com/wil/test/Test.java
@@ -1,15 +1,6 @@
 package com.wil.test;
 
-import com.wil.util.HttpClientUtil;
 import com.wil.util.Spider;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by wil on 2017/10/23.
